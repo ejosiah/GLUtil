10a8e14ef91207752c22506557ad90dfaaa5b278
Josiah Ebhomenye <josiah.ebhomenye@news.co.uk>
2020-04-21 17:08:03 +0100

    added position tracking to map

---
+++ GLUtil/shaders/viewport.point.glsl
@@ -0,0 +1,37 @@
+#version 450 core
+#pragma degub(on)
+#pragma optimize(off)
+
+layout(points) in;
+
+layout(points, max_vertices = 1) out;
+
+uniform int id;
+
+in VERTEX {
+	smooth vec3 position;
+	smooth vec3 normal;
+	smooth vec2 texCoord;
+	smooth vec4 color;
+} vertex_in[];
+
+out VERTEX {
+	smooth vec3 position;
+	smooth vec3 normal;
+	smooth vec2 texCoord;
+	smooth vec4 color;
+} vertex_out;
+
+
+void main(){
+	for(int i = 0; i < gl_in.length(); i++){
+		gl_ViewportIndex = id;
+		gl_Position = gl_in[i].gl_Position;
+		vertex_out.position = vertex_in[i].position;
+		vertex_out.color = vertex_in[i].color;
+		vertex_out.texCoord = vertex_in[i].texCoord;
+		vertex_out.normal = vertex_in[i].normal;
+		EmitVertex();
+	}
+	EndPrimitive();
+}
