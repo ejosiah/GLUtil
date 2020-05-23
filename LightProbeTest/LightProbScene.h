932ceb19e3303a0daa3d4585f319b928b883c4b1
Josiah Ebhomenye <josiah.ebhomenye@news.co.uk>
2020-05-23 17:46:52 +0100

    forgot these guys

--- LightProbeTest/LightProbScene.h
+++ LightProbeTest/LightProbScene.h
@@ -24,14 +24,6 @@ public:
 		addShader("skybox", GL_FRAGMENT_SHADER, skybox_frag_shader);
 		addShader("skybox", GL_GEOMETRY_SHADER, skybox_geom_shader);
 
-		//addShader("equi_rect", GL_VERTEX_SHADER, equi_rectangular_map_vert_shader);
-		//addShader("equi_rect", GL_FRAGMENT_SHADER, equi_rectangular_map_frag_shader);
-
-		//addShader("irradiance_convolution", GL_VERTEX_SHADER, irradiance_convolution_vert_shader);
-		//addShader("irradiance_convolution", GL_FRAGMENT_SHADER, irradiance_convolution_frag_shader);
-		//addShader("prefilter", GL_VERTEX_SHADER, specular_convolution_vert_shader);
-		//addShader("prefilter", GL_FRAGMENT_SHADER, speculuar_convolution_frag_shader);
-
 		addShader("render", GL_VERTEX_SHADER, probe_render_vert_shader);
 		addShader("render", GL_FRAGMENT_SHADER, probe_render_frag_shader);
 
@@ -43,10 +35,8 @@ public:
 		addShader("screen", GL_GEOMETRY_SHADER, octahedral_render_geom_shader);
 		addShader("screen", GL_FRAGMENT_SHADER, octahedral_render_frag_shader);
 
-	//	addShader("prefilter", GL_VERTEX_SHADER, getText("light_probe_prefilter.vert"));
-	//	addShader("prefilter", GL_VERTEX_SHADER, screen_vert_shader);
-	//	addShader("prefilter", GL_GEOMETRY_SHADER, getText("light_field_probe_prefilter.geom"));
-	//	addShader("prefilter", GL_FRAGMENT_SHADER, getText("light_field_probe_prefilter.frag"));
+		addShader("lfp_test", GL_VERTEX_SHADER, screen_vert_shader);
+		addShader("lfp_test", GL_FRAGMENT_SHADER, getText("lfp_uniform_test.frag"));
 
 		logger = Logger::get("probe");
 		camInfoOn = true;
@@ -57,13 +47,12 @@ public:
 		activeCamera().collisionTestOff();
 		activeCamera().setMode(Camera::FIRST_PERSON);
 		activeCamera().setPosition({ 0, 1, 3 });
+		setForeGroundColor(WHITE);
 		sponza = new Model("C:\\Users\\" + username + "\\OneDrive\\media\\models\\Sponza\\sponza.obj", false, 20);
 		lightPos = { 0, 2.43, 0.6 };
 		shadowMap = OminiDirectionalShadowMap{ 5, lightPos, 2048, 2048 };
 		lightObj = new Sphere(0.3, 20, 20, WHITE);
 		probePos = { 0, 2.43, 0 };
-		min_loc = Sphere{ 1.0, 50, 50, WHITE };
-		max_loc = Sphere{ 1.0, 50, 50, WHITE };
 
 		auto center = (sponza->bound->max() + sponza->bound->min()) * 0.5f;
 
@@ -72,159 +61,55 @@ public:
 		Y = new Vector{ {0, 1, 0}, center, 1.0, GREEN };
 		Z = new Vector{ {0, 0, 1}, center, 1.0, BLUE };
 
-		auto fragmentShader = getText("light_field_probe_input.frag");
-
-		auto config = probeConfig();
-		//probe = Probe{ this, probePos, 1.0f, config, fragmentShader };
-
-		//probes.emplace_back(this, vec3{ 5, 2.43, 5 }, 1.0f, config, fragmentShader);
-		//probes.emplace_back(this, vec3{ 5, 2.43, -5 }, 1.0f, config, fragmentShader);
-		//probes.emplace_back(this, vec3{ -5, 2.43, -5 }, 1.0f, config, fragmentShader);
-		//probes.emplace_back(this, vec3{ -5, 2.43, 5 }, 1.0f, config, fragmentShader);
-		//probes.emplace_back(this, probePos, 1.0f, config, fragmentShader);
-
-		initProbes();
-
 		shadowMap.update(lightPos);
 		shadowMap.capture([&] {
 			sponza->defautMaterial(false);
 			shade(sponza);
 		});
 
-		//probe.capture([&] {
-		//	renderScene();
-		//});
+		//for (auto& probe : probes) {
+		//	probe.capture([&] {
+		//		renderScene();
+		//	});
+		//}
 
-		for (auto& probe : probes) {
-			probe.capture([&] {
-				renderScene();
-			});
-		}
-
-		cube = Cube{ 1, WHITE, {}, false };
-		cube.defautMaterial(false);
+		//cube = Cube{ 1, WHITE, {}, false };
+		//cube.defautMaterial(false);
 		quad = ProvidedMesh{ screnSpaceQuad() };
 		quad.defautMaterial(false);
-		initOctahedral();
-		initMeanDistanceProbeGrid();
-		initIrradiaceProbeGrid();
+		//initOctahedral();
+		//initMeanDistanceProbeGrid();
+		//initIrradiaceProbeGrid();
 	//	skybox = SkyBox{ this, irradianceProbeGrid.texture() };
-	}
-
-	void initProbes() {
-		auto dim = sponza->bound->max() - sponza->bound->min();
-		probeStep = dim / probeCount;
-		probeStep.y *= 0.5;
-		vec3 probeStart = sponza->bound->min() + probeStep * vec3(0.5, 0.8, 0.5);
-		auto fragmentShader = getText("light_field_probe_input.frag");
-
-	//	probeCount = { 2, 2, 2 };
-		//probeStep = { 0.1, 0.1, 0.1 };
-		//auto probeStart = probePos;
-
-		auto config = probeConfig();
-		for (int z = 0; z < probeCount.z; z++) {
-			for (int y = 0; y < probeCount.y; y++) {
-				for (int x = 0; x < probeCount.x; x++) {
-					vec3 location = vec3(x, y, z) * probeStep + probeStart;
-					probes.emplace_back(this, location, 1.0f, config, fragmentShader);
-				}
-			}
-		}
-	}
-
-	void initOctahedral() {
-		
-		octahedral = FrameBuffer{ octahedralConfig() };
-
-		vec4 colors[] = { RED, BLUE, YELLOW, GREEN, CYAN };
-
-		octahedral.use([&] {
-			shader("octahedral")([&] {
-				for (int layer = 0; layer < probes.size(); layer++) {
-					octahedral.attachTextureFor(layer);
-					auto& probe = probes[layer];
-					glBindTextureUnit(0, probes[layer].texture(0));
-					glBindTextureUnit(1, probes[layer].texture(1));
-					glBindTextureUnit(2, probes[layer].texture(2));
-					shade(quad);
-				}
-			});
+		lightFieldProbes = LightFieldProbes{ lfpConfig(), this };
+		lightFieldProbes.init();
+		lightFieldProbes.capture([&] {
+			renderScene();
 		});
 
+		LightFieldSurface c = lightSurface;
+		LightFieldSurface a;
+		LightFieldSurface b;
 	}
 
-	void initIrradiaceProbeGrid() {
-		
-		
-		mat4 projection = glm::perspective(glm::half_pi<float>(), 1.0f, 0.1f, 1000.0f);
-		glm::mat4 views[6]{
-			lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
-			lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
-			lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f,  1.0f,  0.0f), glm::vec3(0.0f,  0.0f,  1.0f)),
-			lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f,  0.0f), glm::vec3(0.0f,  0.0f, -1.0f)),
-			lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f,  0.0f,  1.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
-			lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f,  0.0f, -1.0f), glm::vec3(0.0f, -1.0f,  0.0f))
 
-		};
-
-		irradianceProbeGrid = FrameBuffer{ irradianceProbeGridConfig() };
-		irradianceProbeGrid.use([&] {
-			shader("lfp_prefilter")([&] {
-				send("numSamples", 2048);
-				send("lobeSize", 1.0f);
-				send("irradiance", true);	// TODO try subroutine
-				shader("lfp_prefilter").sendUniformMatrix4fv("views", 6, false, value_ptr(views[0]));
-				shader("lfp_prefilter").sendUniformMatrix4fv("projection", 1, false, value_ptr(projection));
-				for (int layer = 0; layer < probes.size(); layer++) {
-					send("layer", layer);
-				//	irradianceProbeGrid.attachTextureFor(layer);
-					auto& probe = probes[layer];
-					glBindTextureUnit(0, probes[layer].texture(0));
-					shade(cube);
-				}
-			});
-		});
-	}
-
-	void initMeanDistanceProbeGrid() {
-
-		mat4 projection = glm::perspective(glm::half_pi<float>(), 1.0f, 0.1f, 1000.0f);
-		glm::mat4 views[6]{
-			lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
-			lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
-			lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f,  1.0f,  0.0f), glm::vec3(0.0f,  0.0f,  1.0f)),
-			lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, -1.0f,  0.0f), glm::vec3(0.0f,  0.0f, -1.0f)),
-			lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f,  0.0f,  1.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
-			lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f,  0.0f, -1.0f), glm::vec3(0.0f, -1.0f,  0.0f))
-
-		};
-
-		meanDistanceProbeGrid = FrameBuffer{ meanDistanceProbeGridConfig() };
-		meanDistanceProbeGrid.use([&] {
-			shader("lfp_prefilter")([&] {
-				send("numSamples", 128);
-				send("lobeSize", 1.0f);
-				send("irradiance", false);	// TODO try subroutine
-				shader("lfp_prefilter").sendUniformMatrix4fv("views", 6, false, value_ptr(views[0]));
-				shader("lfp_prefilter").sendUniformMatrix4fv("projection", 1, false, value_ptr(projection));
-				for (int layer = 0; layer < probes.size(); layer++) {
-					send("layer", layer);
-					//	irradianceProbeGrid.attachTextureFor(layer);
-					auto& probe = probes[layer];
-					glBindTextureUnit(0, probes[layer].texture(2));
-					shade(cube);
-				}
-				});
-			});
-	}
 
 	void display() override {
 	//	probes[0].render();
 	//	renderOctahedral();
 	//	renderRealScene();
 	//	skybox.render();
-		renderIrradiance();
+	//	renderIrradiance();
+	//	lightFieldProbes.renderProbe(28);
+	//	lightFieldProbes.renderOctahedrals();
+	//	lightFieldProbes.renderLowResDistanceProbe();
+	//	lightFieldProbes.renderIrradiance();
+	//	lightFieldProbes.renderMeanDistance();
+		shader("lfp_test")([&] {
+			send("layer", 21);
+			lightFieldProbes.sendTo(shader("lfp_test"));
+			shade(quad);
+		});
 	}
 
 	void renderRealScene() {
@@ -240,31 +125,12 @@ public:
  			auto model = glm::translate(glm::mat4{ 1 }, lightPos);
 			send(activeCamera(), model);
 			shade(lightObj);
-			for (auto& probe : probes) {
-				shade(probe);
-			}
-
-			send("M", translate(mat4{ 1 }, sponza->bound->min()));
-			shade(min_loc);
-			send("M", translate(mat4{ 1 }, sponza->bound->max()));
-			shade(max_loc);
+			shade(lightFieldProbes);
 
 			send("M", mat4{ 1 });
 			shade(X);
 			shade(Y);
 			shade(Z);
-		});
-	}
-
-	void renderOctahedral() {
-
-		shader("screen")([&] {
-			send("isDistance", attachment == 2);
-			send("numLayers", int(probes.size()));
-			glBindTextureUnit(0, octahedral.texture(attachment));
-			glBindTextureUnit(1, irradianceProbeGrid.texture());
-			glBindTextureUnit(2, meanDistanceProbeGrid.texture());
-			shade(quad);
 		});
 	}
 
@@ -274,20 +140,6 @@ public:
 		send("lightPos", lightPos);
 		send("shadowOn", shadowOn);
 		shade(sponza);
-	}
-
-	void renderIrradiance() {
-		glDepthFunc(GL_LEQUAL);
-		cube.defautMaterial(false);
-		shader("lfp_irradiance_render")([&] {
-			send("layer", 0);
-			//glBindTextureUnit(0, irradianceProbeGrid.texture());
-			send("isDistance", true);
-			glBindTextureUnit(0, meanDistanceProbeGrid.texture());
-			send(activeCamera());
-			shade(cube);
-		});
-		glDepthFunc(GL_LESS);
 	}
 
 	void update(float dt) override{
@@ -305,103 +157,6 @@ public:
 		}
 	}
 
-	FrameBuffer::Config probeConfig() {
-		auto config = FrameBuffer::Config{ probeRes, probeRes };
-		config.fboTarget = GL_FRAMEBUFFER;
-		config.depthAndStencil = true;
-		config.depthTest = true;
-		config.stencilTest = false;
-
-		for (int i = 0; i < 3; i++) {
-			auto attachment = FrameBuffer::Attachment{};
-			attachment.magFilter = GL_NEAREST;
-			attachment.minfilter = GL_NEAREST;
-			attachment.wrap_t = attachment.wrap_s = attachment.wrap_r = GL_CLAMP_TO_EDGE;
-			attachment.texTarget = GL_TEXTURE_CUBE_MAP;
-			attachment.internalFmt = GL_RGBA32F;
-			attachment.fmt = GL_RGBA;
-			attachment.type = GL_FLOAT;
-			attachment.attachment = GL_COLOR_ATTACHMENT0 + i;
-			attachment.texLevel = 0;
-			config.attachments.push_back(attachment);
-		}
-
-		
-		return config;
-	}
-
-	FrameBuffer::Config octahedralConfig() {
-		auto config = FrameBuffer::Config{ probeRes, probeRes };
-		config.fboTarget = GL_FRAMEBUFFER;
-		config.depthAndStencil = false;
-		config.depthTest = false;
-		config.stencilTest = false;
-
-		for (int i = 0; i < 3; i++) {
-			auto attachment = FrameBuffer::Attachment{};
-			attachment.magFilter = GL_NEAREST;
-			attachment.minfilter = GL_NEAREST;
-			attachment.wrap_t = attachment.wrap_s = attachment.wrap_r = GL_CLAMP_TO_EDGE;
-			attachment.texTarget = GL_TEXTURE_2D_ARRAY;
-			attachment.internalFmt = GL_R11F_G11F_B10F;
-			attachment.fmt = GL_RGBA;
-			attachment.type = GL_FLOAT;
-			attachment.attachment = GL_COLOR_ATTACHMENT0 + i;
-			attachment.texLevel = 0;
-			attachment.numLayers = probes.size();
-			config.attachments.push_back(attachment);
-		}
-
-		config.attachments[2].internalFmt = GL_RG16F;
-		config.attachments[2].fmt = GL_RG;
-
-		return config;
-	}
-
-	FrameBuffer::Config irradianceProbeGridConfig() {
-		auto config = FrameBuffer::Config{ 128, 128 };
-		config.fboTarget = GL_FRAMEBUFFER;
-		config.depthAndStencil = false;
-		config.depthTest = false;
-		config.stencilTest = false;
-
-		auto attachment = FrameBuffer::Attachment{};
-		attachment.magFilter = GL_NEAREST;
-		attachment.minfilter = GL_NEAREST;
-		attachment.wrap_t = attachment.wrap_s = attachment.wrap_r = GL_CLAMP_TO_EDGE;
-		attachment.texTarget = GL_TEXTURE_CUBE_MAP_ARRAY;
-		attachment.internalFmt = GL_R11F_G11F_B10F;
-		attachment.fmt = GL_RGBA;
-		attachment.type = GL_FLOAT;
-		attachment.attachment = GL_COLOR_ATTACHMENT0;
-		attachment.texLevel = 0;
-		attachment.numLayers = probes.size();
-		config.attachments.push_back(attachment);
-
-		return config;
-	}
-	FrameBuffer::Config meanDistanceProbeGridConfig() {
-		auto config = FrameBuffer::Config{ 128, 128 };
-		config.fboTarget = GL_FRAMEBUFFER;
-		config.depthAndStencil = false;
-		config.depthTest = false;
-		config.stencilTest = false;
-
-		auto attachment = FrameBuffer::Attachment{};
-		attachment.magFilter = GL_NEAREST;
-		attachment.minfilter = GL_NEAREST;
-		attachment.wrap_t = attachment.wrap_s = attachment.wrap_r = GL_CLAMP_TO_EDGE;
-		attachment.texTarget = GL_TEXTURE_CUBE_MAP_ARRAY;
-		attachment.internalFmt = GL_RG32F;
-		attachment.fmt = GL_RG;
-		attachment.type = GL_FLOAT;
-		attachment.attachment = GL_COLOR_ATTACHMENT0;
-		attachment.texLevel = 0;
-		attachment.numLayers = probes.size();
-		config.attachments.push_back(attachment);
-
-		return config;
-	}
 
 	LightFieldProbes::Config lfpConfig() {
 		auto dim = sponza->bound->max() - sponza->bound->min();
@@ -409,15 +164,16 @@ public:
 		config.probeCount = { 8, 2, 4 };
 		config.probeStep = dim / config.probeCount;
 		config.probeStep.y *= 0.5;
-		config.startProbeLocation = sponza->bound->min() + probeStep * vec3(0.5, 0.8, 0.5);
+		config.startProbeLocation = sponza->bound->min() + config.probeStep * vec3(0.5, 0.8, 0.5);
 		config.captureFragmentShader = getText("light_field_probe_input.frag");
-		config.resolution = 1024;
+		config.resolution = 512;
 		config.irradiance.numSamples = 2048;
 		config.irradiance.lobeSize = 1.0f;
 		config.irradiance.resolution = 128;
 		config.meanDistance.numSamples = 128;
 		config.meanDistance.lobeSize = 1.0f;
 		config.meanDistance.resolution = 128;
+		config.lowResolutionDownsampleFactor = 8;
 
 		return config;
 	}
@@ -428,10 +184,6 @@ private:
 	Probe probe;
 	Cube cube;
 	GLsizei probeRes = 512;
-	vector<Probe> probes;
-	FrameBuffer octahedral;
-	FrameBuffer irradianceProbeGrid;
-	FrameBuffer meanDistanceProbeGrid;
 	Model* sponza;
 	OminiDirectionalShadowMap shadowMap;
 	vec3 lightPos;
@@ -442,10 +194,6 @@ private:
 	Vector* X;
 	Vector* Y;
 	Vector* Z;
-	vec3 probeCount{ 8, 2, 4 };
-	vec3 probeStep{ 0.2, 0.2, 0.2 };
-	Sphere min_loc;
-	Sphere max_loc;
 	LightFieldSurface lightSurface;
 	LightFieldProbes lightFieldProbes;
 	float roughness = 0;
---
+++ LightProbeTest/lfp_uniform_test.frag
@@ -0,0 +1,40 @@
+#version 450 core
+
+#pragma include("lightFieldProbeModel.glsl")
+#pragma include("octahedral.glsl")
+
+const float PI = 3.1415926535897932384626422832795028841971;
+const float TWO_PI = 6.2831853071795864769252867665590057683943;
+
+vec3 direction_from_spherical(vec2 uv)
+{
+	float phi = uv.x * TWO_PI;
+	float theta = uv.y * PI;
+
+	return vec3(
+		sin(phi) * sin(theta) * -1.0,
+		cos(theta) * -1.0,
+		cos(phi) * sin(theta) * -1.0
+	);
+}
+
+uniform int layer = 0;
+
+in ncl_PerVertex{
+	smooth vec2 texCoord;
+};
+
+out vec4 fragColor;
+
+void main(){
+//	vec3 color = texture(lightFieldSurface.radianceProbeGrid.sampler, vec3(texCoord, layer)).rgb;
+//	vec3 color = (texture(lightFieldSurface.normalProbeGrid.sampler, vec3(texCoord, layer)) * lightFieldSurface.normalProbeGrid.readMultiplyFirst + lightFieldSurface.normalProbeGrid.readAddSecond).rgb;
+//	vec3 color = texture(lightFieldSurface.distanceProbeGrid.sampler, vec3(texCoord, layer)).rrr;
+//	vec3 color = texture(lightFieldSurface.lowResolutionDistanceProbeGrid.sampler, vec3(texCoord, layer)).rrr;
+//	vec3 dir = direction_from_spherical(texCoord);
+	vec3 dir = octDecode(texCoord * vec2(2.0) - vec2(1.0));
+	vec3 color = texture(lightFieldSurface.irradianceProbeGrid.sampler, vec4(dir, layer)).rgb;
+//	vec3 color = texture(lightFieldSurface.meanDistProbeGrid.sampler, vec4(dir, layer)).rrr;
+//	color /= (color + 1);
+	fragColor = vec4(color, 1);
+}
